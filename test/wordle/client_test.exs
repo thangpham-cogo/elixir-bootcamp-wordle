97f58d3eb897a941f7e1386673367424f35711e3
thang.pham <thang.pham@cogo.co>
2022-08-22 21:24:47 +1200

    test client get_user_input

--- test/wordle/client_test.exs
+++ test/wordle/client_test.exs
@@ -4,6 +4,17 @@ defmodule Wordle.ClientTest do
 
   import ExUnit.CaptureIO
 
+  test "trim an lowercase user input" do
+    expected = "hello"
+
+    actual =
+      capture_io([input: " HeLLo  ", capture_prompt: false], fn ->
+        IO.write(Client.get_user_input("Enter your guess"))
+      end)
+
+    assert actual == expected
+  end
+
   test "end_game displays the correct message" do
     message = capture_io(fn -> Client.end_game("secret") end)
 
